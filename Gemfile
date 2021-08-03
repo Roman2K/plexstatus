a20a1f8a47a1871d5d5c8e1062fb17ca7703ea85
Roman Le Négrate <roman.lenegrate@gmail.com>
2021-08-03 12:41:46 +0200

    initial commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+gem 'utils-ruby', github: 'Roman2K/utils-ruby'
+gem 'webrick'
---
+++ main.rb
@@ -0,0 +1,34 @@
+require 'utils'
+require 'webrick'
+
+class Tautulli
+  def initialize(uri, api_key:)
+    @http = Utils::SimpleHTTP.new URI(uri).tap { |u|
+      u.path = u.path.to_s.sub(%r[/+$], "") + "/api/v2"
+      u.query = URI.encode_www_form out_type: "json", apikey: api_key
+    }, json: true
+  end
+
+  def cmd(name)
+    res = @http.get([cmd: name]).fetch "response"
+    res.fetch("result") == "success" or raise \
+      "unexpected response (#{res.fetch("result")}): #{res.fetch("message")}"
+    res.fetch "data"
+  end
+end
+
+conf = Utils::Conf.new "config.yml"
+cli = Tautulli.new conf[:url], api_key: conf[:api_key]
+srv = WEBrick::HTTPServer.new Port: 8080
+srv.mount_proc "/sesscount" do |req, res|
+  unless req.path_info.empty?
+    res.status = 404
+    next
+  end
+  res.status = 200
+  res['Content-Type'] = "application/json"
+  res.body = JSON.dump cli.cmd("get_activity").fetch("sessions").size
+end
+
+trap 'INT' do srv.shutdown end
+srv.start
